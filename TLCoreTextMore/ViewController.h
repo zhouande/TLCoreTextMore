fe42ab30d6adb9aed2ecccfa904dd3630bd26ffb
andezhou <zhoujunfeng007@126.com>
2015-08-12 16:50:10 +0800

    Initial Commit

---
+++ TLCoreTextMore/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TLCoreTextMore
+//
+//  Created by andezhou on 15/8/12.
+//  Copyright (c) 2015年 andezhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TLCoreTextMore/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TLCoreTextMore
+//
+//  Created by andezhou on 15/8/12.
+//  Copyright (c) 2015年 andezhou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
